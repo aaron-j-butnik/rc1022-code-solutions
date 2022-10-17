306f01e7d6e190d4d7138ed75341b4753f92e0f7
Aaron Butnik <aaronjbutnik@gmail.com>
2022-10-17 17:58:14 +0000

    Changed all const variables to var variables.

--- javascript-primitives-and-variables/main.js
+++ javascript-primitives-and-variables/main.js
@@ -1,20 +1,20 @@
-const fullName = 'Aaron Butnik';
+var fullName = 'Aaron Butnik';
 console.log('value of fullName:', fullName);
 
-const isCool = true;
+var isCool = true;
 console.log('value of isCool:', isCool);
 
-const totalPets = 0;
+var totalPets = 0;
 console.log('value of totalPets:', totalPets);
 
 console.log('typeof fullName:', typeof 'Aaron Butnik');
 console.log('typeof isCool:', typeof true);
 console.log('typeof totalPets:', typeof 0);
 
-const unknown = null;
+var unknown = null;
 console.log('the value of unknown:', unknown);
 console.log('typeof unknown:', typeof unknown);
 
-let never;
+var never;
 console.log('value of never:', never);
 console.log('typeof never:', typeof never);
