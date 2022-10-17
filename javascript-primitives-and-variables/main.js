2fa7f4df7567ebfe54a3367c7ef8c50c05b12d36
Aaron Butnik <aaronjbutnik@gmail.com>
2022-10-17 16:40:44 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,20 @@
+const fullName = 'Aaron Butnik';
+console.log('value of fullName:', fullName);
+
+const isCool = true;
+console.log('value of isCool:', isCool);
+
+const totalPets = 0;
+console.log('value of totalPets:', totalPets);
+
+console.log('typeof fullName:', typeof 'Aaron Butnik');
+console.log('typeof isCool:', typeof true);
+console.log('typeof totalPets:', typeof 0);
+
+const unknown = null;
+console.log('the value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+let never;
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
