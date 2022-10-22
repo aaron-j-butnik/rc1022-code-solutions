d261da5c8d6b9d083058735e32a206fe259f8c80
Aaron Butnik <aaronjbutnik@gmail.com>
2022-10-22 01:53:16 +0000

    Saving progress.

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -3,16 +3,18 @@
 /* Pseudo Code:
   Goal: Return a string containing the characters of the parameter in reverse order.
 
-  1. Use a variable with an empty string as a placeholder.
-  2. Use a 'for-loop' to loop over the string parameter.
-  3. Set condition of 'for-loop' to be half of the length of the string parameter.
+  1. Declare a variable and assign an empty string as the value.
+  2. Use a 'for-loop'.
+  3. Set initialization equal to the length of the parameter minus 1.
+  4. Set condition of 'for-loop' to be greater than or equal to 0.
+  5. Set final expression to decrement after each iteration.
+  6. In the 'for-loop' code block, use the addition assignment operator to concatenate and assign the parameter at i to the variable assigned with the empty string.
 */
 
-// function reverseWord(word) {
-//   var placeholder = '';
-//   for (var i = 0; i < word.length / 2; i++) {
-//     placeholder = word[i];
-//     word[word.length - 1] += word[i];
-//   }
-//   return placeholder;
-// }
+function reverseWord(word) {
+  var newString = '';
+  for (var i = word.length - 1; i >= 0; i--) {
+    newString += word[i];
+  }
+  return newString;
+}
