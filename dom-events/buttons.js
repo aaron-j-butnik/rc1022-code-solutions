63afdc6a76d77872ca0e8957a471ed5c11e5977f
Aaron Butnik <aaronjbutnik@gmail.com>
2022-10-24 20:38:58 +0000

    Competed dom-events exercise.

--- dom-events/buttons.js
+++ dom-events/buttons.js
@@ -1,7 +1,7 @@
 function handleClick(event) {
   console.log('button clicked');
   console.log('event:', event);
-  console.log('event.target:', event.currentTarget);
+  console.log('event.target:', event.target);
 }
 
 var $buttonClicked = document.querySelector('.click-button');
@@ -10,7 +10,7 @@ $buttonClicked.addEventListener('click', handleClick);
 function handleMouseover(event) {
   console.log('button hovered');
   console.log('event:', event);
-  console.log('event.target:', document.querySelector('.hover-button'));
+  console.log('event.target:', event.target);
 }
 
 var $buttonHovered = document.querySelector('.hover-button');
@@ -19,7 +19,7 @@ $buttonHovered.addEventListener('mouseover', handleMouseover);
 function handleDoubleClick(event) {
   console.log('button double clicked');
   console.log('event:', event);
-  console.log('event.target:', document.querySelector('.double-click-button'));
+  console.log('event.target:', event.target);
 }
 
 var $buttonDoubleClicked = document.querySelector('.double-click-button');
