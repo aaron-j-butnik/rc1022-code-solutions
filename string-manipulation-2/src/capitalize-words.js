58a786e41178a43d3103d4279f9db652f46d0d53
Aaron Butnik <aaronjbutnik@gmail.com>
2022-10-28 18:56:13 +0000

    Saving progress on capitalizeWord.

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,17 @@
 /* exported capitalizeWords */
+
+/* Pseudo Code
+
+1. Function definition named capitalizeWord with one parameter, (word);
+2. Use toUpperCase() method of the parameter object being called with no arguments, concatenated with, the toLowerCase() method being called with no arguments, of the slice() method being called with one argument number 1, of the parameter object.
+3. 'If' statement with
+
+*/
+
+// function capitalizeWord(word) {
+//   if (word.toLowerCase() === 'javascript') {
+//     return word[0].toUpperCase() + word.slice(1, 4).toLowerCase() + word[4].toUpperCase() + word.slice(5).toLowerCase();
+//   } else {
+//     return word[0].toUpperCase() + word.slice(1).toLowerCase();
+//   }
+// }
