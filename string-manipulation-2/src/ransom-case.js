339d5c96b14f249f9ccb19a70d392bce0edc0beb
Aaron Butnik <aaronjbutnik@gmail.com>
2022-10-29 00:01:00 +0000

    Saving progress.

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,25 @@
 /* exported ransomCase */
+
+/* Pseudo Code
+
+1. Function definition named ransomCase with one parameter (string).
+2. for loop
+3. conditional if odd lowerCase(), if even toUpperCase()
+4. return
+
+*/
+
+function ransomCase(string) {
+  var strArr = [];
+  for (var i = 0; i < string.length; i++) {
+    if (i % 2 === 0) {
+      strArr.push(string.split('')[i].toLowerCase());
+    } else {
+      strArr.push(string.split('')[i].toUpperCase());
+    }
+  }
+  return strArr.join('');
+}
+
+// }
+// console.log(strArr);
