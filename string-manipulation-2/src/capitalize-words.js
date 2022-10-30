255b854e61ce273bff8c824050e1e15d75ef755f
Aaron Butnik <aaronjbutnik@gmail.com>
2022-10-30 00:49:01 +0000

    Saving progress on capitalize-words.

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,22 @@
 /* exported capitalizeWords */
+
+/* Pseudo Code
+
+1. Function definition named capitalizeWords with one paramater, (words).
+2. Assign the split method of the words object with one argument, string space (' '), to the variable wordsArr.
+3. Assign and empty array iteral to variable wordsCapArr.
+4. 'for-loop' - Initialization: variable i is equal to 0.
+5. 'for-loop' - Condition: variable i is less than the length property of the wordsArr object.
+6. 'for-loop' - Final expression: variable i incremented by one after each iteration.
+7. The push method of the wordsCappArr object is being called with one argument: The toUpperCase() method of the wordsArr object at index i, at index 0 is being called with no arugments, concatenated with the toLowerCase() method of the slice medthod being called with one argument, number 1, of the wordsArr object at index i.
+8. Return the join method of the wordsCapArr object is being called with one argument, string space (' ').
+*/
+
+function capitalizeWords(words) {
+  var wordsArr = words.split(' ');
+  var wordsCapArr = [];
+  for (var i = 0; i < wordsArr.length; i++) {
+    wordsCapArr.push(wordsArr[i][0].toUpperCase() + wordsArr[i].slice(1).toLowerCase());
+  }
+  return wordsCapArr.join(' ');
+}
--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -3,10 +3,12 @@
 /* Pseudo Code
 
 1. Function definition named ransomCase with one parameter (string).
-2. for loop
-3. conditional if odd lowerCase(), if even toUpperCase()
-4. return
-
+2. Empty array literal assigned to variable strArr
+3. 'for-loop', initialization: 0 assigned to var i;
+4. 'for-loop', condition: var i less than length of string object;
+5. 'for-loop', final expression: var i increments by one;
+6. 'If' statement, condition: var i modulus 2 is strickly equal to zero.  This checks for even or odd character in string.
+7. if condition is odd character: push method of strArr object is called with one argument, toLowerCase() method of the split() method of the string object, being called with one argument 'empty string' at index i.
 */
 
 function ransomCase(string) {
