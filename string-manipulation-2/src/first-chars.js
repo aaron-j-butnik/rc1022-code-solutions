5eaa2c2763e56327735ce2b88200694633af6c06
Aaron Butnik <aaronjbutnik@gmail.com>
2022-11-05 20:03:23 +0000

    Changed 'if' statement return to string 'JavaScript' in funciton capitalizeWord(word), moved 'if' statement to before the 'for-loop', and placed 'for-loop' in the 'else' statement for funciton firstChars(length, string) and function lastChars(length,string).

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -4,24 +4,24 @@
 
 1. Function definition named firstChars with two parameters, (length, string).
 2. Assign empty string to variable chars.
-3. 'for-loop' - Initialization: var i is equal to zero.
-              - Condition: var i is less than or equal to parameter length.
-              - Final Expression: var i is incremented by one after each iteration.
-4. 'if' statement condition:
+3. 'if' statement condition:
               - string is strickly equal to empty string ('') or,
               - the length property of the string object is less than the parameter length.
-5. Return string.
+4. Return string.
 6. 'Else' statement:
+        - 'for-loop' - Initialization: var i is equal to zero.
+              - Condition: var i is less than or equal to parameter length.
+              - Final Expression: var i is incremented by one after each iteration.
         - variable chars is concatenated with parameter string at index var i, and the result of the expression is assigned to variable chars.
 7. Return variable chars.
 */
 
 function firstChars(length, string) {
   var chars = '';
-  for (var i = 0; i < length; i++) {
-    if (string === '' || string.length < length) {
-      return string;
-    } else {
+  if (string === '' || string.length < length) {
+    return string;
+  } else {
+    for (var i = 0; i < length; i++) {
       chars += string[i];
     }
   }
