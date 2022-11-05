8f00abafe3eb2eb363ef120a1b69d791c51af7f7
Aaron Butnik <aaronjbutnik@gmail.com>
2022-11-05 23:18:07 +0000

    Fixed typing tutor so underline is only on selected character.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -7,11 +7,13 @@ document.addEventListener('keydown', handleKeyDown);
 function handleKeyDown(event) {
   if ($letters[letterIndex].textContent === event.key) {
     $letters[letterIndex].classList.add('correct');
-    $letters[letterIndex].classList.add('gd-line');
     $letters[letterIndex].classList.remove('incorrect');
+    $letters[letterIndex + 1].classList.add('on-letter');
     letterIndex++;
+    $letters[letterIndex - 1].classList.remove('on-letter');
   } else {
     $letters[letterIndex].classList.add('incorrect');
-    $letters[letterIndex].classList.add('bd-line');
+    $letters[letterIndex].classList.remove('on-letter');
+
   }
 }
