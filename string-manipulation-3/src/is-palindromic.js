53103cced5960be8455ca0470a919f62224e407d
Aaron Butnik <aaronjbutnik@gmail.com>
2022-11-09 07:36:14 +0000

    Saving progress.

---
+++ string-manipulation-3/src/is-palindromic.js
@@ -0,0 +1,18 @@
+/* exported isPalindromic */
+
+function isPalindromic(string) {
+  // TODO:
+  // if the first is equal to the last
+  // if the second is equal to the second-to-last
+  // ...
+
+  // i: 2
+  //   v v
+  // racecar
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] !== string[string.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
