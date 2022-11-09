9b581412d4782805a8d43271a4369aaa7362909b
Aaron Butnik <aaronjbutnik@gmail.com>
2022-11-09 00:53:20 +0000

    Saving Progress on bank.

---
+++ javascript-bank/bank.js
@@ -0,0 +1,47 @@
+/* exported Bank */
+
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+
+  if (Number.isInteger(balance) && balance > 0) {
+    var jeffAccount = new Account(this.nextAccountNumber, holder);
+    jeffAccount.balance = balance;
+    this.accounts.push(jeffAccount);
+    this.nextAccountNumber++;
+    // console.log(jeffAccount);
+    for (var i = 0; i < this.accounts.length; i++) {
+      var number = this.accounts[i].number;
+    }
+    return number;
+  } else {
+    return null;
+  }
+
+};
+
+Bank.prototype.getAccount = function (number) {
+
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+// Bank.prototype.getTotalAssets = function () {
+//   var grandBalance = 0;
+
+//   for (var i = 0; i < this.transactions.length; i++) {
+//     if (this.transactions[i].type === 'deposit') {
+//       totalDeposits += this.transactions[i].amount;
+//     } else {
+//       totalWithdraw += this.transactions[i].amount;
+//     }
+//   }
+//   return totalDeposits - totalWithdraw;
+// };
