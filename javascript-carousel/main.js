7edbdba310168823996be415efa9e12814ab4254
Aaron Butnik <aaronjbutnik@gmail.com>
2022-11-10 07:10:36 +0000

    Completed javascript-carousel.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -4,25 +4,28 @@
 var $rightChev = document.querySelector('.fa-chevron-right');
 var $leftChev = document.querySelector('.fa-chevron-left');
 var $imageChange = document.querySelector('img');
-
-// var $btns = document.querySelectorAll('.fa-circle');
+var $btnMain = document.querySelector('.btn');
+var $btns = document.querySelectorAll('.fa-circle');
 var imageUrls = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
 
-// var btnIndex = 0;
 var imageIndex = 0;
 
 $rightChev.addEventListener('click', handleRightChev);
 $leftChev.addEventListener('click', handleLeftChev);
+$btnMain.addEventListener('click', handleBtn);
 
 function handleRightChev(event) {
-  // btnIndex++;
+
   imageIndex++;
   if (imageIndex === imageUrls.length) {
     imageIndex = 0;
-    // btnIndex = 0;
+
   }
   $imageChange.setAttribute('src', imageUrls[imageIndex]);
-
+  for (var i = 0; i < $btns.length; i++) {
+    $btns[i].classList.remove('bg-circle');
+  }
+  $btns[imageIndex].classList.add('bg-circle');
 }
 
 function handleLeftChev(event) {
@@ -31,16 +34,33 @@ function handleLeftChev(event) {
     imageIndex = 4;
   }
   $imageChange.setAttribute('src', imageUrls[imageIndex]);
+  for (var i = 0; i < $btns.length; i++) {
+    $btns[i].classList.remove('bg-circle');
+  }
+  $btns[imageIndex].classList.add('bg-circle');
 }
 
-// var imageCount = 0;
 setInterval(changeImage, 3000);
 
 function changeImage() {
   imageIndex++;
-  $imageChange.setAttribute('src', imageUrls[imageIndex]);
   if (imageIndex === imageUrls.length) {
     imageIndex = 0;
   }
+  $imageChange.setAttribute('src', imageUrls[imageIndex]);
+  for (var i = 0; i < $btns.length; i++) {
+    $btns[i].classList.remove('bg-circle');
+  }
+  $btns[imageIndex].classList.add('bg-circle');
+}
 
+function handleBtn(event) {
+  if (event.target.matches('i')) {
+    imageIndex = Number(event.target.getAttribute('data-index'));
+    $imageChange.setAttribute('src', imageUrls[imageIndex]);
+    for (var i = 0; i < $btns.length; i++) {
+      $btns[i].classList.remove('bg-circle');
+    }
+    $btns[imageIndex].classList.add('bg-circle');
+  }
 }
