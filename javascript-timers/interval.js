bf6d7f10bb9ecf0e2bcaa9155aabf9e22f3f22bc
Aaron Butnik <aaronjbutnik@gmail.com>
2022-11-10 08:14:14 +0000

    Changed code to make function scalable.

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,27 +1,8 @@
-// var $blastOff = document.querySelector('.countdown-display');
-// $blastOff.addEventListener('click', letsGo);
-
-// function letsGo() {
-//   setInterval(startCountdown, 1000);
-// }
-
-// function startCountdown() {
-//   var $countdown = document.querySelector('.countdown-display');
-//   if ($countdown.textContent === '4') {
-//     $countdown.textContent = '3';
-//   } else if ($countdown.textContent === '3') {
-//     $countdown.textContent = '2';
-//   } else if ($countdown.textContent === '2') {
-//     $countdown.textContent = '1';
-//   } else if ($countdown.textContent === '1') {
-//     $countdown.textContent = '~Earth Beeeelooowww Us~';
-//   }
-// }
-
-var intervalId = setInterval(startCountdown, 1000);
 
 var count = 4;
 var $countdown = document.querySelector('.countdown-display');
+var intervalId = setInterval(startCountdown, 1000);
+
 function startCountdown() {
   count--;
   if (count > 0) {
