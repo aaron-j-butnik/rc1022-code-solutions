4d7d055f7cdab6d73abd472c8d5b96688aed2353
Aaron Butnik <aaronjbutnik@gmail.com>
2022-11-10 08:12:42 +0000

    Changed code to make function more scalable.

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,19 +1,33 @@
-var $blastOff = document.querySelector('.countdown-display');
-$blastOff.addEventListener('click', letsGo);
+// var $blastOff = document.querySelector('.countdown-display');
+// $blastOff.addEventListener('click', letsGo);
 
-function letsGo() {
-  setInterval(startCountdown, 1000);
-}
+// function letsGo() {
+//   setInterval(startCountdown, 1000);
+// }
 
+// function startCountdown() {
+//   var $countdown = document.querySelector('.countdown-display');
+//   if ($countdown.textContent === '4') {
+//     $countdown.textContent = '3';
+//   } else if ($countdown.textContent === '3') {
+//     $countdown.textContent = '2';
+//   } else if ($countdown.textContent === '2') {
+//     $countdown.textContent = '1';
+//   } else if ($countdown.textContent === '1') {
+//     $countdown.textContent = '~Earth Beeeelooowww Us~';
+//   }
+// }
+
+var intervalId = setInterval(startCountdown, 1000);
+
+var count = 4;
+var $countdown = document.querySelector('.countdown-display');
 function startCountdown() {
-  var $countdown = document.querySelector('.countdown-display');
-  if ($countdown.textContent === '4') {
-    $countdown.textContent = '3';
-  } else if ($countdown.textContent === '3') {
-    $countdown.textContent = '2';
-  } else if ($countdown.textContent === '2') {
-    $countdown.textContent = '1';
-  } else if ($countdown.textContent === '1') {
-    $countdown.textContent = '~Earth Beeeelooowww Us~';
+  count--;
+  if (count > 0) {
+    $countdown.textContent = count;
+  } else {
+    $countdown.textContent = '~Earth Beeelooww Us~';
+    clearInterval(intervalId);
   }
 }
