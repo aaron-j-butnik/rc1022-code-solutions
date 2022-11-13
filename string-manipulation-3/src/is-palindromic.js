4bf77311ccda64f53e5dfda1b04865cd358c639d
Aaron Butnik <aaronjbutnik@gmail.com>
2022-11-13 07:29:25 +0000

    Finished isPalindromic, saving progress.

--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -1,16 +1,19 @@
 /* exported isPalindromic */
 
-function isPalindromic(string) {
-  // TODO:
-  // if the first is equal to the last
-  // if the second is equal to the second-to-last
-  // ...
+/* Pseudo Code:
+1. Define function named isPalindromic with one parameter (string);
+2. Assign the replace method of the string object being called with two arguments string space, and empty string, to the variable string.
+3. for loop - Zero assigned to variable i.
+            - i is less than the length property of the string.
+            - i is incremented by one each iteration.
+4. if statement: condition - string object at index i is strickley not equal to string object at length property of string object minus var i minus one.
+  -return false;
+5. Return true.
 
-  // i: 2
-  //   v v
-  // racecar
-  //   v  v
-  // taco cat
+*/
+
+function isPalindromic(string) {
+  string = string.replace(' ', '');
 
   for (var i = 0; i < string.length; i++) {
     if (string[i] !== string[string.length - i - 1]) {
