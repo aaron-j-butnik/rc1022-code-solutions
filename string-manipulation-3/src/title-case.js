e976333a1eecaeb6e1079f830713c7ddb6236def
Aaron Butnik <aaronjbutnik@gmail.com>
2022-11-27 07:54:35 +0000

    Saving Progress.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -7,11 +7,10 @@ function titleCase(title) {
   var words = title.split(' ');
   // console.log('words: ', words);
   for (var i = 0; i < words.length; i++) {
-    var isStartOfSubtitle = false;
+    // var isStartOfSubtitle = false;
 
     // TODO: Set isStartOfSubtitle to true if the previous word ended with a ":" (use .endsWith())
-
-    if (!minorWords.includes(words[i].toLowerCase()) || isStartOfSubtitle) {
+    if (!minorWords.includes(words[i].toLowerCase()) || words[i].endsWith(':')) {
       words[i] = words[i][0].toUpperCase() + words[i].slice(1).toLowerCase();
     } else {
       words[i] = words[i].toLowerCase();
@@ -22,6 +21,16 @@ function titleCase(title) {
     if (words[i] === 'Api') {
       words[i] = 'API';
     }
+    if (words[i] === words[0]) {
+      words[i] = words[i][0].toUpperCase() + words[i].slice(1).toLowerCase();
+    }
+    if (words[i].includes('-')) {
+      var split = words[i].split('-');
+      var splitWord = split[split.length - 1][0].toUpperCase() + split[1].slice(1).toLowerCase();
+      words[i] = split[0] + '-' + splitWord;
+    }
   }
+  // console.log(words.join(' '));
   return words.join(' ');
+
 }
