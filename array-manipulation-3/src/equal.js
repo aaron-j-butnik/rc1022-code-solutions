7d3d0c2dcef436191a66f592279c3dbf818d239a
Aaron Butnik <aaronjbutnik@gmail.com>
2022-12-01 23:24:35 +0000

    Changed equal function, which now uses a for loop with a condition to check if element at index i is strictly not equal to second object at i, return false.

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,21 +1,23 @@
 /* exported equal */
 
 /*
-1. if statment, length property of the first object, not equal to length property of the second object.
+1. If statment, length property of the first object, not equal to length property of the second object.
   return false.
-2. Else if statement, first object at zero not equal to second object at zero.
-  return false.
-3. Return every method of the first object being called with anonymous function with parameter value, return includes method of the second object called with argument value.
+2. For loop, zero assigned to var i, i less than length property of the first object, i++;
+  if statement, condition: first object at index i is strictly not equal to second object at index i.
+    return false;
+3. Return true.
 
 */
 
 function equal(first, second) {
   if (first.length !== second.length) {
     return false;
-  } else if (first[0] !== second[0]) {
-    return false;
   }
-  return first.every(function (value) {
-    return second.includes(value);
-  });
+  for (var i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
 }
