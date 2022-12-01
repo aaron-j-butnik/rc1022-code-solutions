dbb9d858996f24405b89966aac4537b091c6ca6c
Aaron Butnik <aaronjbutnik@gmail.com>
2022-12-01 01:03:32 +0000

    Added new line character.

--- node-fs-writefile/write-rand.js
+++ node-fs-writefile/write-rand.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
 const randNum = String(Math.random());
-fs.writeFile('random.txt', randNum, 'utf8', err => {
+fs.writeFile('random.txt', randNum + '\n', 'utf8', err => {
   if (err) throw err;
 });
