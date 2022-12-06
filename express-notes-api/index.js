ed4a5e03623a7003dede84535054c00f26b5107e
Aaron Butnik <aaronjbutnik@gmail.com>
2022-12-06 04:32:51 +0000

    Saving progress.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,27 +1,32 @@
-// const dataJSON = require('./data.json');
-// const express = require('express');
-// const app = express();
+const dataJSON = require('./data.json');
+const express = require('express');
+const app = express();
 
-// app.get('/api/notes', (req, res) => {
-//   const notes = [];
-//   for (const prop in dataJSON.notes) {
-//     notes.push(dataJSON.notes[prop]);
-//   }
-//   res.json(notes);
-// });
+app.get('/api/notes', (req, res) => {
+  const notes = [];
+  for (const prop in dataJSON.notes) {
+    notes.push(dataJSON.notes[prop]);
+  }
+  res.json(notes);
+});
 
-// app.get('api/notes/:id', (req, res) => {
-//   const id = Number(req.param.id);
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  if (id < 0) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (dataJSON.notes[id] === undefined) {
+    res.status(404).json({ error: `cannot find note with id ${id}` });
+  } else {
+    res.status(200).json(dataJSON.notes[id]);
+  }
+});
 
-//   if (id < 0) {
-//     res.sendStatus(400).json('"error": "id must be a positive integer"');
-//   } else if (id === dataJSON.notes[id]) {
-//     res.sendStatus(200).json(dataJSON.notes[id]);
-//   } else {
-//     res.sendStatus(404).json(`"error: cannot find note with id ${id}"`); 2;
-//   }
-// });
+app.listen(3000, () => {
+  // console.log('Listening on port 3000');
+});
 
-// app.listen(3000, () => {
-//   // console.log('Listening on port 3000');
-// });
+app.use(express.json());
+
+app.post('/api/notes', (req, res) => {
+
+});
