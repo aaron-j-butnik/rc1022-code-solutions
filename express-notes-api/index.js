1a9daeef4dc51a8410b0c02595dcc2df5a1af92d
Aaron Butnik <aaronjbutnik@gmail.com>
2022-12-06 06:36:08 +0000

    Saving progress.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,5 +1,6 @@
 const dataJSON = require('./data.json');
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
 app.get('/api/notes', (req, res) => {
@@ -21,12 +22,30 @@ app.get('/api/notes/:id', (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  // console.log('Listening on port 3000');
-});
-
 app.use(express.json());
 
 app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  const id = dataJSON.nextId;
+  dataJSON.nextId++;
+  newNote.id = id;
+  dataJSON.notes[id] = newNote;
 
+  if (!newNote.content) {
+    res.status(400).json({ error: 'Content is a required field.' });
+  } else {
+    const createdNote = JSON.stringify(dataJSON, null, 2);
+    fs.writeFile('der/data.json', createdNote, err => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An unexpected error occurred.' });
+      }
+      res.status(201).json(dataJSON.notes[id]);
+    });
+  }
+});
+
+app.listen(3000, () => {
+  //  eslint-disable-next-line no-console
+  console.log('Listening on port 3000');
 });
