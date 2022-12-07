8a8a9331309e6c8f60fffae94631c85c4a3190ea
Aaron Butnik <aaronjbutnik@gmail.com>
2022-12-07 07:14:07 +0000

    Completed express-notes-api challenge.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -35,7 +35,53 @@ app.post('/api/notes', (req, res) => {
     res.status(400).json({ error: 'Content is a required field.' });
   } else {
     const createdNote = JSON.stringify(dataJSON, null, 2);
-    fs.writeFile('derp/data.json', createdNote, err => {
+    fs.writeFile('data.json', createdNote, err => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An unexpected error occurred.' });
+      } else {
+        res.status(201).json(dataJSON.notes[id]);
+      }
+    });
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+
+  if (id < 0) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (dataJSON.notes[id] === undefined) {
+    res.status(404).json({ error: `cannot find note with id ${id}` });
+  } else {
+    delete dataJSON.notes[id];
+    const deletedNote = JSON.stringify(dataJSON, null, 2);
+    fs.writeFile('data.json', deletedNote, err => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An unexpected error occurred.' });
+      } else {
+        res.sendStatus(204);
+      }
+    });
+  }
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const updatedNote = req.body;
+
+  if (id < 0) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (!updatedNote.content) {
+    res.status(400).json({ error: 'content is a required field' });
+  } else if (dataJSON.notes[id] === undefined) {
+    res.status(404).json({ error: `cannot find note with id ${id}` });
+  } else {
+    updatedNote.id = id;
+    dataJSON.notes[id] = updatedNote;
+    const replacedNote = JSON.stringify(dataJSON, null, 2);
+    fs.writeFile('data.json', replacedNote, err => {
       if (err) {
         console.error(err);
         res.status(500).json({ error: 'An unexpected error occurred.' });
