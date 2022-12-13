2446281e62c8a4b6259bcbb40009ce7f58f2d420
Aaron Butnik <aaronjbutnik@gmail.com>
2022-12-13 00:58:33 +0000

    Saving progress on es6 classes.

---
+++ es6-classes/example-class.js
@@ -0,0 +1,20 @@
+class ExampleClass {
+  constructor() {
+    this.class = 'JavaScript';
+  }
+}
+
+const example = new ExampleClass();
+
+console.log(example);
+
+console.log('typeof example:', typeof example);
+console.log('value of example:', example);
+
+console.log('typeof example.__proto__', typeof example.__proto__);
+console.log('value of example.__proto__', example.__proto__);
+
+console.log('typeof ExampleClass.prototype:', typeof ExampleClass.prototype);
+console.log('value of ExampleClass.prototype:', ExampleClass.prototype);
+
+console.log('example instanceof ExampleClass', example instanceof ExampleClass);
