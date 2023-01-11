f36277b8a7b152cfc45db379972a8d27cd3dea38
Aaron Butnik <aaronjbutnik@gmail.com>
2023-01-11 22:49:33 +0000

    Completed data-structures-linked-lists exercise.

---
+++ data-structures-linked-lists/swap-front.js
@@ -0,0 +1,19 @@
+/* exported swapFront */
+
+function swapFront(list) {
+  if (list.next === null) {
+    return list;
+  }
+  let temp = null;
+  let firstSwap = list.data;
+  let secondSwap = list.next.data;
+
+  temp = firstSwap;
+  firstSwap = secondSwap;
+  secondSwap = temp;
+
+  list.data = firstSwap;
+  list.next.data = secondSwap;
+
+  return list;
+}
